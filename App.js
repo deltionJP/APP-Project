a4f5a62fdcaac00cae37a1daf19630c03a57528f
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2018-10-01 10:54:08 +0200

    ..

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import TopStyle from '../assets/style/TopStyles.js';
+import { StyleSheet, Text, View,Image } from 'react-native';
+// import TopStyle from 'assets/style/TopStyle.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <View style={{flex:1}}>
+                  <Image
+                      style={{
+                          // backgroundColor: '#ccc',
+                          flex: 5,
+                          position: 'absolute',
+                          width: '100%',
+                          height: '100%',
+                          justifyContent: 'center',
+                      }}
+                      source={require('./assets/img/senshagen-beeldmerk.jpg')}
+                  />
+                  <View style={{backgroundColor: 'rgba(255, 255, 255, 0.5)', flex: 1}}></View>
+        </View>
     );
   }
 }
