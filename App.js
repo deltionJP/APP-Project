3cd27aad3c4e342833fa8dab0264a299ba96d208
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2018-11-12 10:18:01 +0100

    no message

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View,Image,TextInput,TouchableOpacity ,Button } from 'react-native';
 // import TopStyle from 'assets/style/TopStyle.js';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 export default class App extends React.Component {
     constructor(){
             super();
             this.state={
-                text:'PZ'
+                text:'PZ',
+                sensData: ''
 
             }
         }
@@ -16,14 +18,24 @@ export default class App extends React.Component {
        // })
        console.log(this.state.text);
      }
-
+     componentDidMount(){
+         this.ding();
+     }
     ding = async() =>{
             console.log("hallo");
-            let currentWeather = 'https://services1.arcgis.com/3YlK2vfHGZtonb1r/arcgis/rest/services/RIVM_Sensors_Zwolle_(gegevens_per_uur_UTC0)/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+            let currentWeather = 'https://services1.arcgis.com/3YlK2vfHGZtonb1r/arcgis/rest/services/RIVM_Sensors_Zwolle_(gegevens_per_uur_UTC0)/FeatureServer/0/query?where=label%20%3D%20%27PZ001%27&outFields=*&outSR=4326&f=json';
             // let currentWeather = 'https://services1.arcgis.com/3YlK2vfHGZtonb1r/arcgis/rest/services/KNMI_Sensors_Zwolle_(gegevens_per_uur)/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
             let response = await fetch (currentWeather);
             let jsonObject = await response.json();
-            console.log(jsonObject);
+            this.setState({
+                sensData: jsonObject
+            });
+            console.log(this.state.sensData.features);
+            // console.log(jsonObject.features[0].attributes.label);
+            var dd = this.state.sensData.features.attributes.find(obj=>{
+                    return obj.label === "PZ001";
+                });
+                console.log(dd);
     }
 
   render() {
